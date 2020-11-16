270b3e8691441419ea4978639da0aa8465d03419
haim7736 <haim7736@naver.com>
2020-11-16 15:57:12 +0900

    게임잼

---
+++ Assets/InGame.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGame : MonoBehaviour
+{
+
+    public int myNum = 10;
+    public Text CountText;
+    public Text FavorabilityText;
+
+    public GameObject pit;
+    public GameObject one;
+    public GameObject two;
+
+    float timer;
+    float waitingTime;
+
+    public int damage = 1;
+
+    private bool FoodCheck;
+    public int Favorability;
+
+    private int FavorabilityCh;
+    bool check;
+
+
+    void Start()
+    {
+        CountText.text = "Count:" + myNum;
+        pit.SetActive(false);
+        two.SetActive(false);
+
+        timer = 0.0f;
+        waitingTime = 2f;
+
+        FoodCheck = false;
+
+        check = false;
+    }
+
+    void Update()
+    {
+        Debug.Log(damage);
+        CountText.text = "Count:" + myNum;
+        FavorabilityText.text = "호감도:" + Favorability;
+        if (myNum <= 0)
+        {
+            one.SetActive(false);
+            two.SetActive(true);
+            myNum = 50;
+        }
+        if (FoodCheck)
+        {
+            timer += Time.deltaTime;
+            if (timer > waitingTime)
+            {
+                pit.SetActive(false);
+                timer = 0;
+                FoodCheck = false;
+            }
+        }
+
+        if(FavorabilityCh / 100 > 0)
+        {
+            check = true;
+            FavorabilityCh = 0;
+        }
+
+        if(check)
+        {
+            damage = damage + (Favorability / 100);
+            check = false;
+        }
+    
+    }
+
+    public void Click()
+    {
+        myNum-= damage;
+    }
+
+    public void Foood()
+    {
+        if (timer <= 0)
+        {
+            pit.SetActive(true);
+            FoodCheck = true;
+            Favorability += 50;
+            FavorabilityCh += 50;
+
+        }
+      
+
+      
+    }
+}
